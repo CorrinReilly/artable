3ece0e9326ddd9b1e0149b69d0811b7389492466
Corrin Reilly <corrin.reilly@gmail.com>
2020-08-28 14:54:55 +0100

    Initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Corrin Reilly on 27/08/2020.
+//  Copyright © 2020 Corrin Reilly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
